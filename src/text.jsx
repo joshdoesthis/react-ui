999b8602b54b448a00cd1408600dc4db8144c4ce
Josh Wilson <joshdoesthis@icloud.com>
2023-06-25 18:27:26 +1200

    basic elements and styles

--- src/text.jsx
+++ src/text.jsx
@@ -1,38 +1,23 @@
 import React, { createElement } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-export const Text = ({ children, varient = 'span', tw = '' }) => {
-  const styles = {
-    base: `mt-2 mb-2`,
-    varient: {
-      h1: `text-4xl font-bold`,
-      h2: `text-3xl font-bold`,
-      h3: `text-2xl font-bold`,
-      h4: `text-xl font-bold`,
-      h5: `text-lg font-bold`,
-      h6: `text-base font-bold`,
-      ol: `list-decimal list-inside`,
-      ul: `list-disc list-inside`,
-      p: `text-base`,
-      span: `
-        text-base
-        mt-0 mb-0
-      `
-    }
+export const Text = ({ children, tw = '', varient = 'span' }) => {
+  const style = {
+    h1: `block my-4 text-4xl font-bold`,
+    h2: `block my-4 text-3xl font-bold`,
+    h3: `block my-4 text-2xl font-bold`,
+    h4: `block my-4 text-xl font-bold`,
+    h5: `block my-4 text-lg font-bold`,
+    h6: `block my-4 text-base font-bold`,
+    ol: `block my-4 list-decimal list-inside`,
+    ul: `block my-4 list-disc list-inside`,
+    p: `block my-4 text-base`,
+    span: `inline text-base`
   }
 
-  const Varient = () =>
-    createElement(
-      varient,
-      {
-        className: twMerge(`
-        ${styles.base}
-        ${styles.varient[varient]}
-        ${tw}
-      `)
-      },
-      children
-    )
-
-  return <Varient />
+  return createElement(
+    varient,
+    { className: twMerge(`${style[varient]} ${tw}`) },
+    children
+  )
 }
