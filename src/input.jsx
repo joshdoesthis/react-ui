358b26d0cea6da5c661155e8ff7b4657457a299a
Josh Wilson <joshdoesthis@icloud.com>
2023-06-25 18:29:14 +1200

    input variants and basic styles

--- src/input.jsx
+++ src/input.jsx
@@ -1,77 +1,75 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export const Input = ({
-  children,
-  id = '',
-  type = 'text',
-  change = () => {},
   blur = () => {},
-  value = '',
+  change = () => {},
   error = '',
+  id = '',
   size = 'base',
-  tw = ''
+  tw = '',
+  varient = 'text',
+  value = ''
 }) => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus()
+      ref.current.innerText = value
+    }
+  }, [ref])
+
   const style = {
     base: `
       bg-zinc-100 dark:bg-zinc-800
-      text-zinc-800 dark:text-zinc-100
       border-1 border-zinc-300 dark:border-zinc-600
       data-[error=true]:border-red-500 dark:data-[error=true]:border-red-500
-      rounded
       outline-none
+      rounded
+      text-zinc-800 dark:text-zinc-100
       w-full
     `,
     size: {
       sm: `
-        text-sm
         px-2 py-1
+        text-sm
       `,
       base: `
-        text-base
         px-4 py-2
+        text-base
       `,
       lg: `
-        text-lg
         px-6 py-3
+        text-lg
       `
     }
   }
 
-  if (type === 'textarea') {
+  if (varient === 'multiline') {
     return (
-      <textarea
-        id={id}
-        className={twMerge(`
-          ${style.base}
-          ${style.size[size]}
-          ${tw}
-          flex-grow
-          border-0
-          p-0
-          resize-none
-        `)}
-        onChange={change}
-        onBlur={blur}
-        value={value}
+      <div
+        className={twMerge(`${style.base} ${style.size[size]} ${tw}`)}
+        contentEditable
         data-error={error}
+        id={id}
+        onBlur={blur}
+        onInput={change}
+        ref={ref}
+        suppressContentEditableWarning
       />
     )
   }
 
   return (
     <input
-      id={id}
-      type={type}
-      className={twMerge(`
-        ${style.base}
-        ${style.size[size]}
-        ${tw}
-      `)}
-      onChange={change}
-      onBlur={blur}
-      value={value}
+      className={twMerge(`${style.base} ${style.size[size]} ${tw}`)}
       data-error={error}
+      id={id}
+      onBlur={blur}
+      onChange={change}
+      type={varient}
+      value={value}
     />
   )
 }
