a02a291ea8d73f1daca5263c630f52e635a71bab
Joshua Wilson <joshdoesthis@icloud.com>
2023-07-27 20:23:33 +1200

    remove style

--- src/markdown.jsx
+++ src/markdown.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { Text } from './text'
 import MDX from 'markdown-to-jsx'
 
-export const Markdown = ({ children, style = '' }) => {
+export const Markdown = ({ children }) => {
   return (
     <MDX
-      className={style}
       options={{
         createElement: (type, props, children) => {
           return <Text {...{ ...props, [type]: true }}>{children}</Text>
